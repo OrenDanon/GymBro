b654f644fc502476bb4dfabd0bc9a484a42abd49
OrenDanon <oren1414111@gmail.com>
2024-04-18 00:51:52 +0300

    fixed an issue with the root div

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RootCmp } from './root-cmp'
-import './index.css'
+import './assets/styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
+
 
 export function HomePage() {
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
 
 return (
-<div style={{backgroundColor: "brown", width: '100px', height: '100px', display: 'none'}}>
-<button onClick={() => navigate('/about')}>Go to About</button>
+    <div className="homepage-container">
+        <h1>hi</h1>
+        <div></div>
+{/* <div style={{backgroundColor: "brown", width: '100px', height: '100px', display: 'none'}}>
+<button onClick={() => navigate('/about')}>Go to About</button> */}
 </div>
+    // </div>
 )
 }
