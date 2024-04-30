5218abee613f8485fa0333ac17b514eb8fb54cb9
OrenDanon <oren1414111@gmail.com>
2024-04-30 21:54:11 +0300

    homepage skeleton implemented - base HTML structured

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,18 +1,22 @@
 // import { useNavigate } from 'react-router-dom'
 
-
 export function HomePage() {
-    // const navigate = useNavigate()
+  // const navigate = useNavigate()
 
-
-return (
-    <div className='homepage-container'>
-        <div className="main-header">
-        <h1>hi</h1>
-        </div>
-{/* <div style={{backgroundColor: "brown", width: '100px', height: '100px', display: 'none'}}>
-<button onClick={() => navigate('/about')}>Go to About</button> */}
-</div>
-    // </div>
-)
+  return (
+    <main className='homepage-container'>
+      <section className='homepage-top'>
+        <header className='app-header'>
+          <h1>hi</h1>
+        </header>
+        <div className='promo-pic'></div>
+        <div className='top-half-content'></div>
+      </section>
+      <section className='homepage-bottom'>
+        <div className="bottom-half-content"></div>
+        <article className="feature-details"></article>
+        <footer className="auth-section"></footer>
+      </section>
+    </main>
+  )
 }
