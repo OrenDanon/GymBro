c84c76a080476bdecfbc1aa46921a1dd3735b14f
OrenDanon <oren1414111@gmail.com>
2024-07-08 23:39:52 +0300

    centered the inspiration -text span

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ export function HomePage() {
             )
         }, 3000)
         return () => clearInterval(interval)
-    }, [])
+    })
 
     return (
         <main className="homepage-container">
